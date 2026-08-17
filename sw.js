ee0f1fe7a71f37a86ed06f85294f5fc1815f6148
tinnxq-alt <tinnxq@gmail.com>
2026-08-17 19:44:11 +0800

    add offline cache service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='emergency-care-assistant-v03';
+const ASSETS=['./','./index.html','./styles.css','./enhancements.css','./data.js','./app.js','./enhancements.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return resp;}).catch(()=>caches.match('./index.html'))));});
