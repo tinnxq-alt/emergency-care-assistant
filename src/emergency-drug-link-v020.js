cf5d944fb6098e9c63665dda6b14aa5641bebd16
tinnxq-alt <tinnxq@gmail.com>
2026-08-24 19:37:25 +0800

    add emergency drug linkage layer for v0.20

---
+++ src/emergency-drug-link-v020.js
@@ -0,0 +1,19 @@
+export const EMERGENCY_DRUG_LINK_V020 = {
+  'cardiac-arrest': {
+    crashCart: [
+      '盐酸肾上腺素',
+      '盐酸胺碘酮',
+      '利多卡因'
+    ],
+    note: '药物显示需结合指南场景，不等同于库存即推荐'
+  },
+  'hypoglycemia': {
+    wardRelated: [
+      '胰岛素',
+      '降糖药'
+    ],
+    crashCart: [
+      '葡萄糖'
+    ]
+  }
+};
