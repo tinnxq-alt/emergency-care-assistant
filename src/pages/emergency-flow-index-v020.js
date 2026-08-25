826784f941f38acb74b45bd718e637087b362919
tinnxq-alt <tinnxq@gmail.com>
2026-08-25 17:16:19 +0800

    Add emergency flow index v0.20

---
+++ src/pages/emergency-flow-index-v020.js
@@ -0,0 +1,13 @@
+// v0.20 emergency flow registry
+export const emergencyFlows = [
+  'cardiac-arrest',
+  'shock',
+  'respiratory-distress',
+  'chest-pain',
+  'altered-consciousness',
+  'hypoglycemia',
+  'seizure',
+  'infection',
+  'bleeding',
+  'abdominal-pain'
+];
