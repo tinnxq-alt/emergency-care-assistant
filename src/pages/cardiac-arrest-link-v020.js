284f66ddd5298ea4830392140f9c264ecbf9129d
tinnxq-alt <tinnxq@gmail.com>
2026-08-25 17:00:40 +0800

    feat: add cardiac arrest drug linkage v0.20

---
+++ src/pages/cardiac-arrest-link-v020.js
@@ -0,0 +1,14 @@
+// 急救流程-药物关联
+window.EMERGENCY_FLOW_LINKS = window.EMERGENCY_FLOW_LINKS || {};
+window.EMERGENCY_FLOW_LINKS.cardiacArrest = {
+  title: '心跳骤停',
+  crashCart: ['肾上腺素','胺碘酮','利多卡因'],
+  sections: [
+    '快速判断',
+    '立即处理',
+    'CPR与除颤',
+    '关键药物',
+    '5H5T可逆原因',
+    '抢救记录'
+  ]
+};
